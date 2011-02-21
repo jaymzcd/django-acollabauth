7e780dad11dbc6eb4a28a015aea19fc6ef537092
jaymz campbell <jaymzcd@googlemail.com>
2011-02-21 21:53:48 +0000

    added setup.py for easy_install

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(name='django-acollabauth',
+    version='0.1',
+    description='Automatically create superusers via Active Collab accounts for Django',
+    author='Jaymz Campbell',
+    author_email='jaymz@jaymz.eu',
+    url='http://jaymz.eu/2011/02/automatic-superusers-via-active-collab-for-django/',
+    packages=['acollabauth',],
+)
+

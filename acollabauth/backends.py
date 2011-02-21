8f2748ef9e309bb70773719b9ea8048dae6c1369
jaymz campbell <jaymzcd@googlemail.com>
2011-02-21 21:08:46 +0000

    basically working - creates superusers based on AC login

--- acollabauth/backends.py
+++ acollabauth/backends.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 import urllib
 
 class ActiveCollabBackend(object):
@@ -9,8 +10,36 @@ class ActiveCollabBackend(object):
         self.login_url = settings.AC_URL + '/login'
 
     def authenticate(self, username=None, password=None):
-        pass
+        try:
+            # turn jaymz@u-dox.com into just jaymz
+            email_user = username[:username.index('@')]
+        except ValueError:
+            # AC uses emails to login, so can fail now
+            return None
+
+        # Pass in the form data to our AC url
+        data = {
+            'login[email]': username,
+            'login[password]': password,
+            'login[remember]': 1,
+            'submitted': 'submitted',
+        }
+        response = urllib.urlopen(self.login_url, urllib.urlencode(data))
+        if 're_route=dashboard' in response.geturl():
+            # The data allowed us to login to AC ok
+            try:
+                user = User.objects.get(username=email_user)
+            except User.DoesNotExist:
+                user = User(username=email_user, email=username)
+                user.set_password(password)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+            return user
+        return None
 
     def get_user(self, user_id):
-        pass
-
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
